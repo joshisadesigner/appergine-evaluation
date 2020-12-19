3aa0178b93a047c4df128052bd2deb126cef2a52
Josh <josue.delacruz@americavoice.com>
2020-12-19 13:49:53 -0600

    Add Table component

---
+++ src/components/Table.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-table striped hover :items="items" :fields="fields"></b-table>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.items)  //undefined;
+  },
+  props: {
+    records: Array,
+  },
+  data() {
+    return {
+      fields: ["title", "publisher", "publicationDate", "isbn", "id"],
+      items: this.records,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
