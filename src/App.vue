f283d4f1d1a4d87cc6372f93fbc1ad879c496663
Josh <josue.delacruz@americavoice.com>
2020-12-19 13:57:38 -0600

    Insert Table component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="app">
-  </div>
+  <b-container id="app">
+    <b-row>
+      <b-col>
+        <div class="table--continer">
+          <Table v-bind:records="records" />
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import Table from './components/Table';
+import recordsData from './mook/records.json'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      records: recordsData,
+    }
+  },
+  components: { Table },
+}
 </script>
 
 <style>
